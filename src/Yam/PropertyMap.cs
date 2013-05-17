5e9ebb97a0e0883fe6033aa3edada279f2f7dfd7
John Kraft <jhkraft@barbecueguy.com>
2013-05-17 12:39:31 -0500

    Massive Refactoring
    Refactored so that all logic is in the Yam static class. This, seems to
    me, allows a more fluent interface.

--- src/Yam/PropertyMap.cs
+++ src/Yam/PropertyMap.cs
@@ -3,51 +3,17 @@ using System.Reflection;
 
 namespace Yams
 {
-    public class PropertyMap
+    public sealed class PropertyMap
     {
-        public PropertyInfo SourceProperty { get; protected set; }
-        public PropertyInfo DestinationProperty { get; protected set; }
-        public Func<object, object> MappingFunction { get; protected set; }
+        public PropertyInfo SourceProperty { get; private set; }
+        public PropertyInfo DestinationProperty { get; private set; }
+        public Func<object, object> MappingFunction { get; private set; }
 
-        public PropertyMap(PropertyInfo sourceProperty, PropertyInfo destinationProperty)
-        {
-            this.SourceProperty = sourceProperty;
-            this.DestinationProperty = destinationProperty;
-        }
-
-        public PropertyMap(PropertyInfo sourceProperty, PropertyInfo destinationProperty, Func<object, object> mappingFunction)
+        internal PropertyMap(PropertyInfo sourceProperty, PropertyInfo destinationProperty, Func<object, object> mappingFunction)
         {
             this.SourceProperty = sourceProperty;
             this.DestinationProperty = destinationProperty;
             this.MappingFunction = mappingFunction;
-        }
-
-        public PropertyMap(PropertyInfo destinationProperty, Func<object, object> mappingFunction)
-        {
-            this.DestinationProperty = destinationProperty;
-            this.MappingFunction = mappingFunction;
-        }
-
-        public void Map(object source, object destination)
-        {
-            if (source == null)
-                throw new ArgumentNullException("source");
-            if (destination == null)
-                throw new ArgumentNullException("destination");
-            if (DestinationProperty == null)
-                throw new InvalidOperationException("DestinationProperty cannot be null");
-
-            if (MappingFunction == null && SourceProperty == null)
-            {
-                var type = DestinationProperty.PropertyType;
-                MappingFunction = src => GetDefaultValue(type);
-            }
-            else if (MappingFunction == null)
-            {
-                MappingFunction = src => SourceProperty.GetValue(src, null);
-            }
-
-            DestinationProperty.SetValue(destination, MappingFunction(source), null);
         }
 
         public override string ToString()
@@ -55,6 +21,7 @@ namespace Yams
             return string.Format("Property {0} using {1} mapping", DestinationProperty.Name, SourceProperty == null ? "custom" : "default");
         }
 
+        // TODO: do i need this?
         private object GetDefaultValue(Type type)
         {
             if (type.IsValueType && Nullable.GetUnderlyingType(type) == null)
