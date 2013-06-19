1d09acc1be87b3f82b8dc0bdec5ce279fbd57416
John Kraft <jhkraft@barbecueguy.com>
2013-06-19 17:17:33 -0500

    Added failing test for Lists of CLR types

---
+++ tests/YamTestClasses/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YamTestClasses
+{
+    public class Object1
+    {
+        public string Name { get; set; }
+        public List<int> Scores { get; set; }
+    }
+
+    public class Object2
+    {
+        public string Name { get; set; }
+        public List<int> Scores { get; set; }
+    }
+
+    public class Object3
+    {
+        public string Name { get; set; }
+        public List<decimal> Scores { get; set; }
+    }
+}
