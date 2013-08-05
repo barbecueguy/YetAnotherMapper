90f51b7398d5a599aea4940b61475ff31738240b
John Kraft <jhkraft@barbecueguy.com>
2013-08-05 17:13:06 -0500

    Added implicit conversion from TypeMap to TypeMap<T,U>

--- src/Yam/TypeMap.cs
+++ src/Yam/TypeMap.cs
@@ -18,5 +18,28 @@ namespace Yams
         private readonly List<PropertyMap> propertyMaps = new List<PropertyMap>();
     }
 
-    public class TypeMap<TSource, TDestination> { }
+    public class TypeMap<TSource, TDestination>
+    {
+        public Type SourceType { get { return this.typeMap.SourceType; } }
+        public Type DestinationType { get { return this.typeMap.DestinationType; } }
+        public List<PropertyMap> PropertyMaps { get { return this.typeMap.PropertyMaps; } }
+
+        public TypeMap()
+        {
+            this.typeMap = new TypeMap(typeof(TSource), typeof(TDestination));
+        }
+
+        public TypeMap(TypeMap typeMap)
+        {
+            this.typeMap = typeMap;
+        }
+
+        public static implicit operator TypeMap<TSource, TDestination>(TypeMap typeMap)
+        {
+            TypeMap<TSource, TDestination> newTypeMap = new TypeMap<TSource, TDestination>(typeMap);
+            return newTypeMap;
+        }        
+
+        private readonly TypeMap typeMap;
+    }
 }
